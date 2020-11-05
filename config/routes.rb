572103a1ee154036c66eae94fd2164705bf5ac7a
joseph <robhaywood12@gmail.com>
2020-11-05 11:52:39 -0600

    changed project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
-  resources :portfolio_pieces
+  resources :portfolio_pieces, except: [:show, :index]
+  get 'project/:id', to: 'portfolio_pieces#show', as: 'portfolio_pieces_show'
+  get 'projects', to: 'portfolio_pieces#index'
+
   get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  #instead of get 'pages/about' we can do
+  get 'about', to: 'pages#about' # visit via url/about
+  get 'contact', to: 'pages#contact'
+
+  
+
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Redict site by default to pages/home
+  root to: 'pages#home'
 end
