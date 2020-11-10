ee87eab15b5f390ba38d0f5c8bd37f7717a80b5e
Joseph <robhaywood12@gmail.com>
2020-11-10 17:54:18 -0600

    add name for new/edit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # adding custom fields that we adding to signup/reg form, in addition to email & pass
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
