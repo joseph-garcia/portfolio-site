efea3747ed44d9aa58f53808a21606b2b49b417e
Joseph <robhaywood12@gmail.com>
2020-11-10 15:20:07 -0600

    add login/logout and configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolio_pieces, except: [:show, :index, :new]
   get 'java-items', to: 'portfolio_pieces#java'
   get 'project/:id', to: 'portfolio_pieces#show', as: 'portfolio_pieces_show'
